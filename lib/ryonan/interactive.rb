85f9c8cbb5b6e9ee582d1bc9ae7464279bf91a9c
tigberd <tigberd@gmail.com>
2015-03-05 07:15:54 +0900

    Refactor extract method

--- lib/ryonan/interactive.rb
+++ lib/ryonan/interactive.rb
@@ -21,7 +21,7 @@ module Ryonan
         end
       end
 
-      def input_template_name
+      def read_template_name
         name = Readline.readline('Input the destination directory name -> ')
         if name == Config.template_dir_name
           puts "#{Config.template_dir_name} will overlap with the template directory name"
@@ -31,21 +31,32 @@ module Ryonan
         end
       end
 
-      def input_configs(template_root)
+      def read_configs(template_root)
         puts 'Set the value to the variable in the template'
-        variables = File.open("#{template_root}/#{Config.template_dir_name}/#{Config.config_file_name}").readlines.map(&:chomp)
+        variables = File.open(config_file_path(template_root)).readlines.map(&:chomp)
         variables.reduce({}) do |acc, line|
-          if line.match(/.+=/)
-            (variable_name, default) = line.split('=')
-            input = Readline.readline("    #{variable_name} (Default:#{default}) -> ")
-            variable_value = input.empty? ? default : input
-          else
-            variable_name = line
-            variable_value = Readline.readline("    #{variable_name} -> ")
-          end
+          (variable_name, variable_value) = read_config(line)
           acc[variable_name.to_sym] = variable_value
           acc
         end
+      end
+      
+      private 
+      
+      def config_file_path(template_root)
+        "#{template_root}/#{Config.template_dir_name}/#{Config.config_file_name}"
+      end
+      
+      def read_config(line)
+        if line.match(/.+=/)
+          (variable_name, default) = line.split('=')
+          input = Readline.readline("    #{variable_name} (Default:#{default}) -> ")
+          variable_value = input.empty? ? default : input
+        else
+          variable_name = line
+          variable_value = Readline.readline("    #{variable_name} -> ")
+        end
+        [variable_name, variable_value]
       end
     end
   end
